a5855cf896973532ab065528f1a1cbdfdda5f651
kortirso <kortirso@gmail.com>
2020-08-01 18:07:14 +0300

    modify consumer

--- config/initializers/consumers/judge.rb
+++ config/initializers/consumers/judge.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
-channel = RabbitMq.consumer_channel
-queue = channel.queue('judge', durable: true)
+Rails.application.config.after_initialize do
+  channel = RabbitMq.consumer_channel
+  queue = channel.queue('judge', durable: true)
 
-queue.subscribe(manual_ack: true) do |delivery_info, _properties, payload|
-  JSON.parse(payload)
+  queue.subscribe(manual_ack: true) do |delivery_info, _properties, payload|
+    JSON.parse(payload)
 
-  channel.ack(delivery_info.delivery_tag)
+    channel.ack(delivery_info.delivery_tag)
+  end
 end
